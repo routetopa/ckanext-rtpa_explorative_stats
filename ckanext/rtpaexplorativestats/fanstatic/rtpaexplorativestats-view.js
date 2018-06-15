334536ef6b0f748c395906684e540985394a53c3
Arek Stasiewicz <arekstasiewicz@mail.com>
2018-06-15 17:16:51 +0100

    js update + cleanup

--- ckanext/rtpaexplorativestats/fanstatic/rtpaexplorativestats-view.js
+++ ckanext/rtpaexplorativestats/fanstatic/rtpaexplorativestats-view.js
@@ -1,61 +1,50 @@
 this.ckan.module('rtpaexplorativestats_view', function(jQuery, _) {
-	return{
-		initialize: function() {
+    return{
+        initialize: function() {
             jQuery.proxyAll(this, /_on/);
             this.el.ready(this._onReady);
-		},
-		_onReady: function() {
-			console.log(this.options.resource);
-			var BoxPlotData=JSON.parse(this.options.resource);
-			
-			//var SummaryDataJSON=JSON.parse(this.options.resource)[1]
-			//var SummaryData=JSON.parse(SummaryDataJSON)
-			//console.log("Summary")
-			//console.log(SummaryData);
-			
-			//console.log(BoxPlotData);
-			this.summaryTable();
-			this.renderBoxPlot(BoxPlotData);
-			//
-					
-		},
-		renderBoxPlot: function(data){
-			//console.log("Here...");
-            //console.log(data);
+        },
+        _onReady: function() {
+            console.log(this.options.resource);
+            var BoxPlotData=JSON.parse(this.options.resource.replace(/\bNaN\b/g, "null"));
+
+            //var SummaryDataJSON=JSON.parse(this.options.resource)[1]
+            //var SummaryData=JSON.parse(SummaryDataJSON)
+
+            this.summaryTable();
+            this.renderBoxPlot(BoxPlotData);
+
+        },
+        renderBoxPlot: function(data){
             boxPlotData=data;
-			if(boxPlotData != false)
-			{
-				var BoxPlotArray=[];
-				if (boxPlotData.length>0)
-				{
-					for (var i=0 ; i<boxPlotData.length; i++)
-					{
-						var element = boxPlotData[i];
-						var tempElement={
-							y: element[0],
-							type: 'box',
-							name: element[1]};
-						BoxPlotArray.unshift(tempElement);
-					}
-				}
-				Plotly.newPlot('Boxplot', BoxPlotArray);
+            if(boxPlotData != false)
+            {
+                var BoxPlotArray=[];
+                if (boxPlotData.length>0)
+                {
+                    for (var i=0 ; i<boxPlotData.length; i++)
+                    {
+                        var element = boxPlotData[i];
+                        var tempElement={
+                            y: element[0],
+                            type: 'box',
+                            name: element[1],
+                            boxpoints: 'Outliers'};
+                        BoxPlotArray.unshift(tempElement);
+                    }
+                }
+                Plotly.newPlot('Boxplot', BoxPlotArray);
 
-			}
-			else
-			{
-				$('#Boxplot').text("Error! No numeric data found!");
-			}
-			
-            
-		},
-		summaryTable: function(){			
-			$('#Summary').DataTable();
-		}
-		
-		
-			
-		
-		
-	};
+            }
+            else
+            {
+                $('#Boxplot').text("Error! No numeric data found!");
+            }
+
+        },
+        summaryTable: function(){
+            $('#Summary').DataTable();
+        }
+
+    };
 });
-
